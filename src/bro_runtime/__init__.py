185edb998df0a4c9d00bee3da47be59a3eb41bd1
ohanyan88-cmd <ohanyan.88@gmail.com>
2026-09-01 08:48:00 +0400

    Expose MIND runtime contracts

--- src/bro_runtime/__init__.py
+++ src/bro_runtime/__init__.py
@@ -1,51 +1,13 @@
 """BRO's provider-independent durable runtime core."""
+from .task_runtime import CompletionEvidence,ConcurrencyConflict,InvalidTransition,RecoveryAssessment,SQLiteTaskStore,TaskContractViolation,TaskRuntime,TaskState
+from .immune import AuthorityDecision,AuthorityEnvelope,AuthorityEvaluator,AuthorityRejected,AuthorityVerdict,CompletionManifest,CompletionNotVerified,CompletionVerdict,EffectRecord,Evidence,EvidenceFreshness,EvidenceLedger,EvidenceRejected,EvidenceValidity,evidence_scope,normalize_boundary_scope
+from .action_runtime import ActionRequest,ActionRuntime,ActionState,AdapterResult,ApprovalRequired,EffectState,RetryBlocked
+from .orchestration import AssignmentRejected,AssignmentState,LeaseGrant,SpecialistAssignment,StaleWorkerResult,Supervisor
+from .supervision import BoundaryViolation,FlowBinding,NextAction,NextStep,SupervisionRejected,TaskSupervisor
+from .mind import Decision,Goal,KnowledgeState,MindRejected,MindRuntime,Plan,PlanStep,SQLiteMindStore
 
-from .task_runtime import (
-    CompletionEvidence,
-    ConcurrencyConflict,
-    InvalidTransition,
-    RecoveryAssessment,
-    SQLiteTaskStore,
-    TaskContractViolation,
-    TaskRuntime,
-    TaskState,
-)
-from .immune import (
-    AuthorityDecision,
-    AuthorityEnvelope,
-    AuthorityEvaluator,
-    AuthorityRejected,
-    AuthorityVerdict,
-    CompletionManifest,
-    CompletionNotVerified,
-    CompletionVerdict,
-    EffectRecord,
-    Evidence,
-    EvidenceFreshness,
-    EvidenceLedger,
-    EvidenceRejected,
-    EvidenceValidity,
-    evidence_scope,
-    normalize_boundary_scope,
-)
-from .action_runtime import ActionRequest, ActionRuntime, ActionState, AdapterResult, ApprovalRequired, EffectState, RetryBlocked
-from .orchestration import AssignmentRejected, AssignmentState, LeaseGrant, SpecialistAssignment, StaleWorkerResult, Supervisor
-from .supervision import BoundaryViolation, FlowBinding, NextAction, NextStep, SupervisionRejected, TaskSupervisor
-
-__all__ = [
-    "CompletionEvidence",
-    "ConcurrencyConflict",
-    "InvalidTransition",
-    "RecoveryAssessment",
-    "SQLiteTaskStore",
-    "TaskContractViolation",
-    "TaskRuntime",
-    "TaskState",
-    "AuthorityDecision", "AuthorityEnvelope", "AuthorityEvaluator", "AuthorityRejected", "AuthorityVerdict",
-    "CompletionManifest", "CompletionNotVerified", "CompletionVerdict", "EffectRecord", "Evidence",
-    "EvidenceFreshness", "EvidenceLedger", "EvidenceRejected", "EvidenceValidity",
-    "evidence_scope", "normalize_boundary_scope",
-    "ActionRequest", "ActionRuntime", "ActionState", "AdapterResult", "ApprovalRequired", "EffectState", "RetryBlocked",
-    "AssignmentRejected", "AssignmentState", "LeaseGrant", "SpecialistAssignment", "StaleWorkerResult", "Supervisor",
-    "BoundaryViolation", "FlowBinding", "NextAction", "NextStep", "SupervisionRejected", "TaskSupervisor",
-]
+__all__=[
+"CompletionEvidence","ConcurrencyConflict","InvalidTransition","RecoveryAssessment","SQLiteTaskStore","TaskContractViolation","TaskRuntime","TaskState",
+"AuthorityDecision","AuthorityEnvelope","AuthorityEvaluator","AuthorityRejected","AuthorityVerdict","CompletionManifest","CompletionNotVerified","CompletionVerdict","EffectRecord","Evidence","EvidenceFreshness","EvidenceLedger","EvidenceRejected","EvidenceValidity","evidence_scope","normalize_boundary_scope",
+"ActionRequest","ActionRuntime","ActionState","AdapterResult","ApprovalRequired","EffectState","RetryBlocked","AssignmentRejected","AssignmentState","LeaseGrant","SpecialistAssignment","StaleWorkerResult","Supervisor","BoundaryViolation","FlowBinding","NextAction","NextStep","SupervisionRejected","TaskSupervisor",
+"Decision","Goal","KnowledgeState","MindRejected","MindRuntime","Plan","PlanStep","SQLiteMindStore"]
