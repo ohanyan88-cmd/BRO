4f6987d6e18177c4c530105618ea5db7c8a7fba1
ohanyan88-cmd <ohanyan.88@gmail.com>
2026-09-01 21:50:17 +0400

    Export service and integration runtime primitives

--- src/bro_runtime/__init__.py
+++ src/bro_runtime/__init__.py
@@ -5,6 +5,9 @@ from .immune import AuthorityDecision,AuthorityEnvelope,AuthorityEvaluator,Autho
 from .action_runtime import ActionRequest,ActionRuntime,ActionState,AdapterResult,ApprovalRequired,EffectState,RetryBlocked
 from .artifact_runtime import ArtifactRecord,ArtifactRejected,ArtifactState,ArtifactStore
 from .provider_adapters import ProviderAdapter,ProviderAdapterRegistry,ProviderAdapterRejected,ProviderHealth
+from .provider_lifecycle import ProviderConnectionState,ProviderLifecycle,ProviderLifecycleRejected,ProviderLifecycleStore
+from .google_provider import CalendarEventProvider,CalendarTarget,GmailSendProvider,GmailTarget,GoogleProviderRejected
+from .service_runtime import BROServiceRuntime,SQLiteWorkQueue,ServiceRejected,WorkItem,WorkState
 from .live_readback import ExternalObservation,LiveReadbackRejected,LiveReadbackRuntime
 from .orchestration import AssignmentRejected,AssignmentState,LeaseGrant,SpecialistAssignment,StaleWorkerResult,Supervisor
 from .supervision import BoundaryViolation,FlowBinding,NextAction,NextStep,SupervisionRejected,TaskSupervisor
@@ -27,15 +30,4 @@ from .multistep_runtime import prepare_multistep,ready_multistep
 from .multistep_execution import complete_multistep,continue_multistep,open_multistep,resume_multistep_with_approval,settle_multistep
 from .replan import ReplanResult,open_replanned_step,replan_from_observation
 
-__all__=[
-"CompletionEvidence","ConcurrencyConflict","InvalidTransition","RecoveryAssessment","SQLiteTaskStore","TaskContractViolation","TaskRuntime","TaskState",
-"AutomationDefinition","AutomationDispatcher","AutomationOccurrence","AutomationRejected","AutomationRuntime","AutomationStatus","MisfirePolicy","OccurrenceState",
-"AuthorityDecision","AuthorityEnvelope","AuthorityEvaluator","AuthorityRejected","AuthorityVerdict","CompletionManifest","CompletionNotVerified","CompletionVerdict","EffectRecord","Evidence","EvidenceFreshness","EvidenceLedger","EvidenceRejected","EvidenceValidity","evidence_scope","normalize_boundary_scope",
-"ActionRequest","ActionRuntime","ActionState","AdapterResult","ApprovalRequired","EffectState","RetryBlocked","ArtifactRecord","ArtifactRejected","ArtifactState","ArtifactStore","ProviderAdapter","ProviderAdapterRegistry","ProviderAdapterRejected","ProviderHealth","ExternalObservation","LiveReadbackRejected","LiveReadbackRuntime","AssignmentRejected","AssignmentState","LeaseGrant","SpecialistAssignment","StaleWorkerResult","Supervisor","BoundaryViolation","FlowBinding","NextAction","NextStep","SupervisionRejected","TaskSupervisor","RestartReconciliation","RestartRecoveryRejected","RestartRecoveryRuntime",
-"Decision","Goal","KnowledgeState","MindRejected","MindRuntime","Plan","SQLiteMindStore","ContextEntry","ContextManifest","NervousRecordRejected","NervousRecordStore","Step","StepState",
-"Approval","ApprovalDecision","ApprovalRegistry","ApprovalRejected","RevocationState","ReferenceIntegrity","ReferenceIntegrityError","GovernedTaskSupervisor",
-"Freshness","Intent","Observation","PerceptionRejected","PerceptionStore","TrustState","MemoryClass","MemoryFreshness","MemoryRecord","MemoryRejected","MemoryRetrieval","MemoryStatus","MemoryStore",
-"ContinuityEnvelope","ContinuityRejected","ContinuityStatus","ContinuityStore","HeartRecord","SelfRecord","Capability","CapabilityKind","CapabilityMatch","CapabilityRegistry","CapabilityRejected","CapabilityStatus",
-"FeetRejected","FeetStore","RouteCheckpoint","RouteState","VoiceInput","VoiceProjection","VoiceRejected","VoiceRuntime","VoiceState","BROKernel","KernelRejected","PreparedFlow","RecoveryView",
-"CheckState","ReadinessCheck","ReadinessMeter","ReadinessReport","RuntimeReadiness","MultiStepRejected","PreparedPlan","PreparedStep","StepRequest","ready_step_refs","validate_graph","prepare_multistep","ready_multistep",
-"open_multistep","settle_multistep","continue_multistep","resume_multistep_with_approval","complete_multistep","ReplanResult","replan_from_observation","open_replanned_step"]
+__all__=[name for name in globals() if not name.startswith('_')]
